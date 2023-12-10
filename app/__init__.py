4e81cd495584b2999af5f2ea00e4c82b0262fba1
Igor Cardoso <iygorsilva@gmail.com>
2023-12-10 10:42:43 -0300

    Add AWS configuration to app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,20 @@
 import sys
 from flask import Flask
+import boto3
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_region = os.getenv("AWS_REGION")
+
+boto3.setup_default_session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=aws_region
+)
 
 sys.path.append('../config')
 
